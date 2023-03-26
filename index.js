50125c30349c1fa9a0099106aeb4ee7e7c15d149
unknown <nlange2021@gmail.com>
2023-03-26 18:00:39 -0500

    all functions

--- index.js
+++ index.js
@@ -3,140 +3,138 @@ const bodyParser = require("body-parser").json();
 const cors = require("cors");
 app = express();
 
+//npm install cors
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get("/ping", (request, response) => {
-  console.log("Ping recieved");
-  response.type("text/plain");
-  response.send("Pong!");
-});
+var imageArray = new Array(0);
+var count = 0;
 
-app.post("/calculate_risk", bodyParser, (request, response) => {
-  risk = {};
+/*should be called by ACP on load!*/
+app.post("/loadImages", (request, response) => {
+  console.log("load request received");
+  loadImages();
+  response.send(imageArray);
+})
 
-  totalScore =
-    request.body.age +
-    request.body.bmi +
-    request.body.bp +
-    request.body.disease;
+app.post("/save", (request, response) => {
+  console.log("save request received");
+  writeImages();
+})
 
-  risk.score = totalScore;
+app.post("/apendImage", (request, response) => {
+  console.log("image received");
+  if(isFull()) {
+    makeSpace();
+  } 
+  append(request.image);
+})
 
-  if (totalScore <= 20) {
-    risk.risk = "Low Risk";
-    risk.color = "bg-success";
-  } else if (totalScore <= 50) {
-    risk.risk = "Moderate Risk";
-    risk.color = "bg-warning";
-  } else if (totalScore <= 75) {
-    risk.risk = "High Risk";
-    risk.color = "bg-danger";
-  } else {
-    risk.risk = "Uninsurable";
-    risk.color = "bg-warning-subtle";
+app.post("/insertImage", (request, response) => {
+  console.log("image and index received");
+  if(isFull()) {
+    makeSpace();
+  } 
+  insert(request.image, request.index);
+})
+
+app.post("/removeImage", (request, response) => {
+  console.log("index received");
+  remove(request.index);
+})
+
+app.get("/getImages", (request, response) => {
+  response.send(imageArray);
+})
+
+function loadImages() {
+  var reader = new FileReader("c:\\data\\myfile.txt");
+
+    var data = reader.read();
+    var i = 0;
+    while(data != -1){
+        var imageArray = data;
+        data = reader.read();
+        i++;
+    }
+}
+
+function writeImages() {
+  var writer = new FileWriter("public/images.txt");
+  writer.write(imageArray);
+  writer.close();
+}
+
+function append(image) {
+  imageArray[count] = image;
+  count++;
+}
+
+function insert(image, index) {
+  for(var i=count-1; i > index; i--) {
+    imageArray[i] = imageArray[i+1];
   }
+  imageArray[index] = image;
+  count++;
+}
 
-  response.type("application/json");
-  response.send(risk);
-});
-
-app.post("/age_to_points", bodyParser, (request, response) => {
-  var output = {};
-  age = request.body.age;
-  output.points = age < 30 ? 0 : age < 45 ? 10 : age < 60 ? 20 : 30;
-
-  if (output.points === 0) output.color = "bg-success";
-  else if (output.points === 10) output.color = "bg-warning";
-  else if (output.points === 20) output.color = "bg-danger";
-  else output.color = "bg-warning-subtle";
-
-  response.type("application/json");
-  response.send(output);
-});
-
-app.post("/bmi_to_points", bodyParser, (request, response) => {
-  var output = {};
-
-  height = request.body.height * 0.0254;
-  weight = request.body.weight / 2.2;
-  bmi = weight / Math.pow(height, 2);
-
-  if (bmi >= 18.5 && bmi < 25) {
-    output.points = 0;
-    output.bmi = "normal";
-    output.color = "bg-success";
-  } else if (bmi < 30) {
-    output.points = 30;
-    output.bmi = "overweight";
-    output.color = "bg-warning";
-  } else {
-    output.points = 75;
-    output.bmi = "other";
-    output.color = "bg-danger";
+function remove(index) {
+  for(var i=index; i < count-1; i++) {
+    imageArray[i] = imageArray[i+1];
   }
+  count--;
+}
 
-  response.type("application/json");
-  response.send(output);
-});
-
-app.post("/bp_to_points", bodyParser, (request, response) => {
-  var output = {};
-
-  systolic = request.body.systolic;
-  diastolic = request.body.diastolic;
-
-  if (systolic > 180 || diastolic > 120) {
-    output.points = 100;
-    output.bp = "crisis";
-  } else if (systolic >= 140 || diastolic >= 90) {
-    output.points = 75;
-    output.bp = "stage 2";
-  } else if (
-    (systolic >= 120 && systolic < 130) ||
-    (diastolic >= 80 && diastolic < 90)
-  ) {
-    output.points = 15;
-    output.bp = "elevated";
-  } else if (systolic >= 130 || diastolic >= 80) {
-    output.points = 30;
-    output.bp = "stage 1";
-  } else if (systolic < 120 && diastolic < 80) {
-    output.points = 0;
-    output.bp = "normal";
-  } else {
-    output.bp = "other";
-    output.points = 0;
+function isFull() {
+  if(count + 1 >= imageArray.length) {
+    return true;
   }
+  return false;
+}
 
-  if (output.points === 0) output.color = "bg-success";
-  else if (output.points === 15) output.color = "bg-warning";
-  else if (output.points === 30) output.color = "bg-danger";
-  else output.color = "bg-warning-subtle";
+function makeSpace() {
+  var newArray = new Array(imageArray.length*2);
+  for(var i=0; i < imageArray.length; i++) {
+    newArray[i] = imageArray[i];
+  }
+  imageArray = newArray;
+}
 
-  response.type("application/json");
-  response.send(output);
-});
 
-app.post("/disease_to_points", bodyParser, (request, response) => {
-  output = { points: 0 };
-  diabetes = request.body.diabetes;
-  cancer = request.body.cancer;
-  alzhe = request.body.alzhe;
 
-  if (cancer) output.points += 10;
-  if (diabetes) output.points += 10;
-  if (alzhe) output.points += 10;
 
-  if (output.points === 0) output.color = "bg-success";
-  else if (output.points === 10) output.color = "bg-warning";
-  else if (output.points === 20) output.color = "bg-danger";
-  else output.color = "bg-warning-subtle";
 
-  response.type("application/json");
-  response.send(output);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Custom 404 page.
 app.use((request, response) => {
